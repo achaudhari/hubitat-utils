9cad6cda41f6d1908e6aa572e8cd6379e546d876
Ashish Chaudhari <ashishsee@gmail.com>
2022-01-16 22:45:29 -0800

    daemon: Refactor email sender and add notification support

--- offload-proc/hubitat-offload-daemon.py
+++ offload-proc/hubitat-offload-daemon.py
@@ -4,7 +4,9 @@ import subprocess
 import time
 import logging
 import argparse
+import random
 import datetime
+import tempfile
 
 import email
 from email.mime.base import MIMEBase
@@ -22,66 +24,94 @@ def get_host_ip_addr():
     cmd = "hostname -i | awk '{print $1}'"
     return subprocess.check_output(cmd, shell=True).strip().decode('utf-8')
 
-RPC_ADDR = get_host_ip_addr()
 RPC_PORT = 4226
 
-def send_page_snapshot_email(email_addr, subject, snapshot_img):
-    msg = MIMEMultipart()
-    msg['To'] = email_addr
-    msg['From'] = f'Hubitat Automation <{email_addr}>'
-    msg['Subject'] = subject
-    message = '<html><body><img src="cid:snapshot_img"/></body></html>'
-    body = MIMEText(message, _subtype='html')
-    msg.attach(body)
-    with open(snapshot_img, 'rb') as fd:
-        mimetype, mimeencoding = mimetypes.guess_type(snapshot_img)
-        if mimeencoding or (mimetype is None):
-            mimetype = 'application/octet-stream'
-        maintype, subtype = mimetype.split('/')
-        if maintype == 'text':
-            attachment = MIMEText(fd.read(), _subtype=subtype)
-        else:
-            attachment = MIMEBase(maintype, subtype)
-            attachment.set_payload(fd.read())
-            email.encoders.encode_base64(attachment)
-        attachment.add_header('Content-ID', '<snapshot_img>')
-        attachment.add_header('Content-Disposition', 'inline',
-            filename = snapshot_img)
-    msg.attach(attachment)
-    eml_fname = '/tmp/snapshot.eml'
-    with open(eml_fname, 'w') as eml_f:
-        eml_f.write(msg.as_string())
-    subprocess.check_call(f'sendmail -t < {eml_fname}', shell=True)
-    os.remove(eml_fname)
+class EmailUtils:
+    @staticmethod
+    def _send_msg(msg):
+        with tempfile.NamedTemporaryFile(suffix='.eml', delete=False) as eml_f:
+            eml_f.write(msg.as_bytes())
+            eml_f.flush()
+            subprocess.check_call(f'sendmail -t < {eml_f.name}', shell=True)
 
-def rpc_email_dashboard(email_addr, dashboard_name, page_wd, page_ht, load_delay):
-    logging.info(f'email_dashboard(email_addr={email_addr}, dashboard_name={dashboard_name}, '
-                 f'page_wd={page_wd}, page_ht={page_ht}, load_delay={load_delay})')
-    logging.info('email_dashboard: Starting browser...')
+    @staticmethod
+    def send_email_text(email_addr, subject, body):
+        msg = MIMEMultipart()
+        msg['To'] = email_addr
+        msg['From'] = f'Automation Bot <{email_addr}>'
+        msg['In-Reply-To'] = msg['From']
+        msg['Subject'] = subject
+        msg['Message-Id'] = f'<{datetime.datetime.now().strftime("%Y%m%d%H%M%S")}.{"%016x" % random.randrange(16 ** 16)}@hauto-offload.local>'
+        body = MIMEText(f'<html><body>{body}</body></html>', _subtype='html')
+        msg.attach(body)
+        EmailUtils._send_msg(msg)
+
+    @staticmethod
+    def send_email_image(email_addr, subject, img_fname):
+        msg = MIMEMultipart()
+        msg['To'] = email_addr
+        msg['From'] = f'Automation Bot <{email_addr}>'
+        msg['In-Reply-To'] = msg['From']
+        msg['Subject'] = subject
+        msg['Message-Id'] = f'<{datetime.datetime.now().strftime("%Y%m%d%H%M%S")}.{"%016x" % random.randrange(16 ** 16)}@hauto-offload.local>'
+        message = '<html><body><img src="cid:img_payload"/></body></html>'
+        body = MIMEText(message, _subtype='html')
+        msg.attach(body)
+        with open(img_fname, 'rb') as fd:
+            mimetype, mimeencoding = mimetypes.guess_type(img_fname)
+            if mimeencoding or (mimetype is None):
+                mimetype = 'application/octet-stream'
+            maintype, subtype = mimetype.split('/')
+            if maintype == 'text':
+                attachment = MIMEText(fd.read(), _subtype=subtype)
+            else:
+                attachment = MIMEBase(maintype, subtype)
+                attachment.set_payload(fd.read())
+                email.encoders.encode_base64(attachment)
+            attachment.add_header('Content-ID', '<img_payload>')
+            attachment.add_header('Content-Disposition', 'inline', filename=img_fname)
+        msg.attach(attachment)
+        EmailUtils._send_msg(msg)
+
+def rpc_email_text(email_addr, subject, email_body):
+    logging.info(f'rpc_email_text(email_addr={email_addr}, subject={subject}, '
+        f'email_body={email_body})')
+    EmailUtils.send_email_text(email_addr, subject, email_body)
+    logging.info('rpc_email_text: Finished successfully')
+
+def rpc_email_web_snapshot(email_addr, subject, page_url, page_wd, page_ht, load_delay):
+    logging.info(f'rpc_email_web_snapshot(email_addr={email_addr}, subject={subject}, '
+        f'page_url={page_url}, page_wd={page_wd}, page_ht={page_ht}, load_delay={load_delay})')
+    # Open headless firefox and resize window
+    logging.info('rpc_email_web_snapshot: Starting browser...')
     options = Options()
     options.headless = True
     driver = webdriver.Firefox(options=options)
     driver.set_window_position(0, 0)
     driver.set_window_size(page_wd, page_ht)
-    logging.info('email_dashboard: Saving dashboard...')
-    driver.get(f'http://{RPC_ADDR}/{dashboard_name}')
+    # Load URL and save screenshot
+    logging.info('rpc_email_web_snapshot: Loading page and saving screenshot...')
+    driver.get(page_url)
     time.sleep(load_delay)
-    dashboard_img_path = f'/tmp/{dashboard_name}.png'
+    dashboard_img_path = tempfile.mktemp(suffix='.png')
     driver.save_screenshot(dashboard_img_path)
-    logging.info('email_dashboard: Sending email...')
-    datestr = datetime.datetime.now().strftime('%d-%b-%Y')
-    send_page_snapshot_email(email_addr, f'INFO: Hubigraph Snapshot ({datestr})', dashboard_img_path)
-    os.remove(dashboard_img_path)
+    # Create email and send
+    logging.info('rpc_email_web_snapshot: Sending email...')
+    EmailUtils.send_email_image(email_addr, subject, dashboard_img_path)
+    # Cleanup
+    os.unlink(dashboard_img_path)
     driver.quit()
-    logging.info('email_dashboard: DONE')
+    logging.info('rpc_email_web_snapshot: Finished successfully')
 
 def rpc_echo(data):
+    logging.info(f'rpc_echo(data={data})')
     return data
 
 @Request.application
 def application(request):
     dispatcher["echo"] = rpc_echo
-    dispatcher["email_dashboard"] = rpc_email_dashboard
+    dispatcher["email_web_snapshot"] = rpc_email_web_snapshot
+    dispatcher["email_text"] = rpc_email_text
 
     response = JSONRPCResponseManager.handle(
         request.data, dispatcher)
@@ -89,6 +119,7 @@ def application(request):
 
 def main():
     parser = argparse.ArgumentParser(description='Hubitat Offload Daemon')
+    parser.add_argument('--rpc-addr', type=str, default=get_host_ip_addr(), help='IP address to bing server to')
     parser.add_argument('--rpc-port', type=int, default=RPC_PORT, help='TCP port to listen on')
     parser.add_argument('--verbose', action='store_true', help='Verbose output')
     args = parser.parse_args()
@@ -96,7 +127,7 @@ def main():
     log_level = logging.DEBUG if args.verbose else logging.INFO
     logging.basicConfig(level=log_level)
 
-    run_simple(RPC_ADDR, args.rpc_port, application)
+    run_simple(args.rpc_addr, args.rpc_port, application)
 
 if __name__ == '__main__':
     main()
